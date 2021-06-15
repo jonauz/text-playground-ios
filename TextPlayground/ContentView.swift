7348e36a386f7e4113fb43ccf2e1300e28dae7da
Jonas Simkus <jonauz@gmail.com>
2021-06-15 22:45:30 +0100

    Initial Commit

---
+++ TextPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextPlayground
+//
+//  Created by Jonas Simkus on 15/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextPlayground/TextPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextPlaygroundApp.swift
+//  TextPlayground
+//
+//  Created by Jonas Simkus on 15/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct TextPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
